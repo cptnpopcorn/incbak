0e6b64742ab0cdc2dc5a0a6fe0f74ac8c9036d4a
Hagen Hentschel <cptnpopcorn@github.com>
2020-11-27 15:10:16 +0100

    re-structure functions
    to have cleaner and safer code: should not move or delete source because of a single typo

--- incbak.sh
+++ incbak.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-backup_level() {
-	local ref="$1"
-	local level="$2"
-	local gen="$((1 << level))"
-	local gendir="$dest.$gen"
+get_target_from_gendir() {
+	local gendir="$1"
+	local target
 
 	for target in $gendir.*; do
 		[ -e "$target" ] || target="$gendir.0"
@@ -12,22 +10,50 @@ backup_level() {
 	done
 
 	[ -e "$target" ] || mkdir "$target" || exit
-	local rev="${target: -1}"
-	local nextrev="$((1 - rev))"
-	
-	if [ "$level" -eq "0" ]; then
-		echo "backup $ref to $gendir.$nextrev with hard-links to $target"
-		rsync -a --delete --link-dest="$target" "$ref/" "$gendir.$nextrev/" || exit
-	else
-		echo "move $ref down to $gendir.$nextrev"
-		mv "$ref" "$gendir.$nextrev"  || exit
-	fi
+	echo "$target"
+}
+
+push_down() {
+	local level="$1"
+	local rev="$2"
+	local target="$3"
 
 	if [[ "$rev" -eq "1" && "$level" -le "$levels" ]]; then
 		backup_level "$target" "$((level + 1))"
 	else
 		rm -r "$target" || exit
-	fi				
+	fi
+
+	return
+}
+
+backup_level() {
+	local ref="$1"
+	local level="$2"
+	local gendir="$dest.$((1 << level))"
+	local target=$(get_target_from_gendir "$gendir")
+	local rev="${target: -1}"
+	local nextrev="$((1 - rev))"
+
+	echo "move $ref down to $gendir.$nextrev"
+	mv "$ref" "$gendir.$nextrev"  || exit
+
+	push_down "$level" "$rev" "$target"
+
+	return
+}
+
+backup_root() {
+	local ref="$1"
+	local gendir="$dest.1"
+	local target=$(get_target_from_gendir "$gendir")
+	local rev="${target: -1}"
+	local nextrev="$((1 - rev))"
+
+	echo "backup $ref to $gendir.$nextrev with hard-links to $target"
+	rsync -a --delete --link-dest="$target" "$ref/" "$gendir.$nextrev/" || exit
+
+	push_down "0" "$rev" "$target"
 
 	return
 }
@@ -43,4 +69,5 @@ if [ -z "$dest" ]; then echo "missing destination" 1>&2; exit 1; fi
 source="$(realpath $source)"
 dest="$(realpath $dest)"
 
-backup_level "$source" 0
+
+backup_root "$source"
