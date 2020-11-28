91ccda9707d2064a6bb22f58ca2b3ffd2252f4f6
Hagen Hentschel <cptnpopcorn@github.com>
2020-11-28 14:08:34 +0100

    improve error output
    especially for use as automated script

--- incbak.sh
+++ incbak.sh
@@ -9,7 +9,7 @@ get_target_from_gendir() {
 		break
 	done
 
-	[ -e "$target" ] || mkdir "$target" || exit
+	[ -e "$target" ] || mkdir "$target" || { echo "Could not create $target" 1>&2; exit 2; }
 	echo "$target"
 }
 
@@ -21,7 +21,7 @@ push_down() {
 	if [[ "$rev" -eq "1" && "$level" -le "$levels" ]]; then
 		backup_level "$target" "$((level + 1))"
 	else
-		rm -r "$target" || exit
+		rm -r "$target" || { echo "Could not remove $target" 1>&2; exit 3; }
 	fi
 
 	return
@@ -36,7 +36,7 @@ backup_level() {
 	local nextrev="$((1 - rev))"
 
 	echo "move $ref down to $gendir.$nextrev"
-	mv "$ref" "$gendir.$nextrev"  || exit
+	mv "$ref" "$gendir.$nextrev"  || { echo "Could not move $ref to $gendir.$nextrev" 1>&2; exit 4; }
 
 	push_down "$level" "$rev" "$target"
 
@@ -53,11 +53,11 @@ backup_root() {
 	local nextrev="$((1 - rev))"
 
 	echo "backup $ref to $gendir.$nextrev with hard-links to $target"
-	rsync -aH --no-inc-recursive --delete --delete-after --link-dest="$target" "$ref/" "$gendir.$nextrev/" || exit
+	rsync -aH --no-inc-recursive --delete --delete-after --link-dest="$target" "$ref/" "$gendir.$nextrev/" || { echo "Could not create primary backup from $ref to $gendir.$nextrev" 1>&2; exit 5; }
 
 	# preserve current source and destination structure as hard-link copies, to identify moved files later
-	rsync -a --delete --link-dest="$ref" --exclude="/$shadow" "$ref/" "$ref/$shadow"
-	rsync -a --delete --link-dest="$gendir.$nextrev" --exclude="/$shadow" "$gendir.$nextrev/" "$gendir.$nextrev/$shadow"
+	rsync -a --delete --link-dest="$ref" --exclude="/$shadow" "$ref/" "$ref/$shadow" || { echo "Could not create shadow for $ref" 1>&2; exit 6; }
+	rsync -a --delete --link-dest="$gendir.$nextrev" --exclude="/$shadow" "$gendir.$nextrev/" "$gendir.$nextrev/$shadow" || { echo "Could not create shadow for $gendir.$nextrev" 1>&2; exit 7; }
 
 	push_down "0" "$rev" "$target"
 
@@ -68,12 +68,11 @@ source="$1"
 dest="$2"
 levels="$3"
 
-if [ ! -d "$source" ]; then echo "missing source" 1>&2; exit 1; fi
-if [ -z "$dest" ]; then echo "missing destination" 1>&2; exit 1; fi
+[ -d "$source" ] || { echo "Missing source" 1>&2; exit 1; }
+[ -z "$dest" ] &&  { echo "Missing destination" 1>&2; exit 1; }
 [ -z "$levels" ] && levels=8
 
 source="$(realpath $source)"
 dest="$(realpath $dest)"
-
 
 backup_root "$source"
